077d6d74506bb976f5168edfc9f8f9e7c9a01cd7
swarangi263 <swarangisatpute263@gmail.com>
2021-07-12 02:33:40 +0530

    first

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $id = auth()->id();
+        $names = User::select('name')
+                ->where('id', '=', $id)
+                ->get();
+        Log::info($names);
+        return view('dashboard', compact('names'));
+    }
+}
