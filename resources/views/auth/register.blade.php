0250307469b0ba9a1f63fc44bc139b5018d3dd3c
swarangi263 <swarangisatpute263@gmail.com>
2021-07-13 21:44:27 +0530

    title

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.app')
+@section('title','Register')
+
 
 @section('content')
 <div class="container">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,8 @@
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
 
 </head>
 
