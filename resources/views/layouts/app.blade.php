158b2bc0562b193f65204c6cfed04d26ef95a5a5
swarangi263 <swarangisatpute263@gmail.com>
2021-07-13 19:08:51 +0530

    welcome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-dark  shadow-sm">
             <div class="container ">
-                <a class="navbar-brand text-white" href="{{ url('dashboard') }}">
+                <a class="navbar-brand text-white" href="{{ url('welcome') }}">
                     {{ ('HOME') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
