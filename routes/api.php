1f45dbcfb98161b1444ab4a3ba885d6779c52517
swarangi263 <swarangisatpute263@gmail.com>
2021-07-23 00:37:22 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Http\Request;
@@ -15,10 +16,17 @@ use App\Http\Controllers\TaskController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('register', [UserAuthController::class, 'register']);
-Route::post('login', [UserAuthController::class, 'login']);
 
-Route::get('test', function(Request $request){return 'hiee' ;});
+// Route::post('register', [UserAuthController::class, 'register']);
+// Route::post('login', [UserAuthController::class, 'login']);
+
+Route::get('test', function(){
+
+    return response()->json([
+        'status' => '1',
+        'message' =>  'This is test'
+    ]);
+});
 
 
 Route::middleware('auth:api')->group(function () {
@@ -26,8 +34,15 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/user', function (Request $req) {
 
             return $req->user();
-
     });
+
+    Route::get('demo', function(){
+        return response()->json([
+        'status' => '1',
+        'message' =>  'This is demo'
+    ]);
+    });
+
     Route::get('/tasks/{id}', [TaskController::class, 'show']);
 
     Route::get('/task', [TaskController::class, 'index']);
